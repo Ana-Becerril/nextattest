22ece7779e2ed3bddea8ce453b937e28ef12b789
Ana Karen Becerril Belio <ana.becerril@agilethought.com>
2022-11-29 18:40:30 -0600

    setup next test

---
+++ pages/careers.js
@@ -0,0 +1,7 @@
+import Link from "next/link"
+
+function Careers(){
+    return <div><h1>Careers page</h1><p>This will be the careers page</p><p><Link href="/"><a>Home</a></Link></p></div>
+}
+
+export default Careers      
---
+++ pages/work.js
@@ -0,0 +1,7 @@
+import Link from "next/link"
+
+function Work(){
+    return <div><h1>Our Work page</h1><p>This will be the Our Work page</p><p><Link>Home</Link></p></div>
+}
+
+export default Work      
